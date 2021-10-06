194a69ed30033fd4a17917fc5e10d36ce61ca763
hoang <nguyenhoang1872011@gmail.com>
2021-10-06 21:23:46 +0700

    Create role table and add relationship into account table

---
+++ database/seeders/20211006134602-seed_role_table.js
@@ -0,0 +1,75 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert("Roles", [
+      {
+        name: "admin",
+        description: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "trainingStaff",
+        description: "trainingStaff",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "trainer",
+        description: "trainer",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "trainee",
+        description: "trainee",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "student",
+        description: "student",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Roles", {
+      [Op.or]: [
+        {
+          name: "admin",
+          description: "admin",
+        },
+        {
+          name: "trainingStaff",
+          description: "trainingStaff",
+        },
+        {
+          name: "trainer",
+          description: "trainer",
+        },
+        {
+          name: "trainee",
+          description: "trainee",
+        },
+        {
+          name: "student",
+          description: "student",
+        },
+      ],
+    });
+  },
+};
