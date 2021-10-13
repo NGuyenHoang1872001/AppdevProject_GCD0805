ba08930f63a85a5f69917ab5c1dddc3a43289662
hoang <nguyenhoang1872011@gmail.com>
2021-10-13 21:27:17 +0700

    new crate and read for model Trainee

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const flash = require("connect-flash");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var adminRouter = require("./routes/admin");
+var staffRouter = require("./routes/staff");
 
 require("dotenv").config();
 
@@ -36,6 +37,7 @@ app.use(flash());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/admin", adminRouter);
+app.use("/staff", staffRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
